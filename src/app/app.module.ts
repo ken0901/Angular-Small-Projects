6c4263ee738e130a90a5b3e24030661458bdfe58
ken0901 <ken5260715@gmail.com>
2024-04-10 20:47:41 -0700

    Remove unnecessary files in the repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppointmentListComponent } from './projects/appointment-list/appointment-list.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        AppointmentListComponent
         
     ],
     imports: [
