8dca4e37ab3ff2a442ca5cb7396d4ff331c77bb0
ken0901 <ken5260715@gmail.com>
2024-04-12 21:35:05 -0700

    CRUD Service and inject to component.

--- src/app/projects/hotel-reservation-app/component/reservation-form/reservation-form.component.ts
+++ src/app/projects/hotel-reservation-app/component/reservation-form/reservation-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from '../../services/reservation.service';
+import { Reservation } from '../../models/reservation';
 
 @Component({
   selector: 'app-reservation-form',
@@ -9,7 +11,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ReservationFormComponent implements OnInit{
   reservationForm: FormGroup = new FormGroup({});
   
-  constructor(private formBuilder: FormBuilder){}
+  constructor(private formBuilder: FormBuilder,
+              private reservationService: ReservationService
+  ){}
 
   ngOnInit(): void {
     this.reservationForm = this.formBuilder.group({
@@ -23,7 +27,8 @@ export class ReservationFormComponent implements OnInit{
   
   onSubmit(){
     if(this.reservationForm.valid){
-      console.log("valid")
+      let reservation: Reservation = this.reservationForm.value;
+      this.reservationService.addReservation(reservation);
     }
   }
 }
