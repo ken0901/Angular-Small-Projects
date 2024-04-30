52abee5da550fde40222dc360d322c163b7a77e9
ken0901 <ken5260715@gmail.com>
2024-04-29 20:10:52 -0700

    Adding products to cart and popup(MatSnackBar)

--- src/app/projects/painting-app/product/product-list/product-list.component.ts
+++ src/app/projects/painting-app/product/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../../models/product';
+import { CartService } from '../../cart/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-list',
@@ -11,11 +13,25 @@ export class ProductListComponent implements OnInit{
 
   products: Product[] = [];
 
-  constructor(private productService: ProductService){}
+  constructor(private productService: ProductService,
+              private cartService: CartService,
+              private snackbar: MatSnackBar){}
   
   ngOnInit(): void {
     this.productService.getProducts().subscribe(data => {
       this.products = data;
+    });
+  }
+
+  addToCart(product: Product): void {
+    this.cartService.addToCart(product).subscribe({
+      next: () => {
+        this.snackbar.open("Product Added to cart!", "", {
+          duration: 2000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top'
+        })
+      }
     });
   }
 
