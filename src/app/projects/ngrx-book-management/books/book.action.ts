558187b56a93c2759a4bf4c8348e12e58c21251c
ken0901 <ken5260715@gmail.com>
2024-05-01 20:25:10 -0700

    Adjusting the reducer for effects

--- src/app/projects/ngrx-book-management/books/book.action.ts
+++ src/app/projects/ngrx-book-management/books/book.action.ts
@@ -3,6 +3,6 @@ import { Book } from "../models/book";
 
 export const AddBook = createAction('[Book] Add Book', props<Book>());
 export const AddBookSuccess = createAction('[Book] Added Book successfully', props<Book>());
-export const AddBookFailure = createAction('[Book] Added Book Failure', props<{error:any}>);
+export const AddBookFailure = createAction('[Book] Added Book Failure', props<{error:any}>());
 
 export const RemoveBook = createAction('[Book] REmove Book', props<{bookId: string}>());
