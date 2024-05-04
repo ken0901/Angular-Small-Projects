652e011d46a085a1af74d287b3cffdeea3f36765
ken0901 <ken5260715@gmail.com>
2024-05-03 21:11:10 -0700

    Inject service to component

--- src/app/projects/social-app/home/home.component.ts
+++ src/app/projects/social-app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Products } from '../interface/types';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent  implements OnInit{
 
+  constructor(private productService: ProductsService){}
+
+  ngOnInit(): void {
+    this.productService.getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 }).subscribe((products: Products) => {
+      console.log(products.items);
+    })
+  }
 }
