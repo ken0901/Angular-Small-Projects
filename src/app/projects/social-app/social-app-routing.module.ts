49614ecc7970b863044553635783df0d2f3abe35
ken0901 <ken5260715@gmail.com>
2024-05-09 21:01:11 -0700

    Module - about us component standalone=false

--- src/app/projects/social-app/social-app-routing.module.ts
+++ src/app/projects/social-app/social-app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path:'about-us', loadChildren: () => import('./modules/about-us/about-us.module').then((m) => m.AboutUsModule)}
 ];
 
 @NgModule({
