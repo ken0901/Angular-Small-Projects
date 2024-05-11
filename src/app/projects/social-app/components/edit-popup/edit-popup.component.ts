5c4afd1d7143169932ccf5aa42f3b25865bda11b
ken0901 <ken5260715@gmail.com>
2024-05-10 21:52:55 -0700

    Adding Form groups

--- src/app/projects/social-app/components/edit-popup/edit-popup.component.ts
+++ src/app/projects/social-app/components/edit-popup/edit-popup.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { Product } from '../../interface/types';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
 import { ButtonModule } from 'primeng/button';
 
@@ -11,9 +11,11 @@ import { ButtonModule } from 'primeng/button';
   templateUrl: './edit-popup.component.html',
   styleUrls: ['./edit-popup.component.css'],
   standalone: true,
-  imports: [DialogModule, CommonModule,FormsModule,RatingModule,ButtonModule]
+  imports: [DialogModule, CommonModule,FormsModule,RatingModule,ButtonModule,ReactiveFormsModule]
 })
 export class EditPopupComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
   @Input() display: boolean = false;
   @Output() displayChange = new EventEmitter<boolean>();
 
@@ -25,11 +27,47 @@ export class EditPopupComponent {
     name: '',
     image: '',
     price: '',
-    rating: 0
+    rating: 0,
   };
 
+  specialCharacterValidator(): ValidatorFn {
+    return (control) => {
+      const hasSpecialCharacter = /[!@#$%^&*()_+-=\[\]{};':"\\|,.<>\/?]+/.test(
+        control.value
+      );
+
+      return hasSpecialCharacter ? { hasSpecialCharacter: true } : null;
+    }
+  }
+
+  productForm = this.formBuilder.group({
+    name: ['',[Validators.required, this.specialCharacterValidator()]],
+    image: [''],
+    price: ['',[Validators.required]],
+    rating: [0]
+  });
+
+  // name: this.product.name,
+  // image: this.product.image,
+  // price: this.product.price,
+  // rating: this.product.rating
+  ngOnChanges() {
+    this.productForm.patchValue(this.product);
+  }
+  
   onConfirm() {
-    this.confirm.emit(this.product);
+    const {name, image, price, rating } = this.productForm.value;
+    this.confirm.emit({
+      name: name || '',
+      image: image || '',
+      price: price || '',
+      rating: rating || 0
+    });
+    //  name: this.productForm.value.name || '',
+    // image: this.productForm.value.image || '',
+    // price: this.productForm.value.price || '',
+    // rating: this.productForm.value.rating || 0
+
     this.display = false;
     this.displayChange.emit(this.display);
   }
