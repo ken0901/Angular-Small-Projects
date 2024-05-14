1f187850a4c9897f7c0ba1b97d6e7533dbed7b48
ken0901 <ken5260715@gmail.com>
2024-05-13 21:32:07 -0700

    Component inputs and the @Input Decorator

--- src/app/projects/angular-university/component-core-directives-pipes/component-core-directives-pipes.component.ts
+++ src/app/projects/angular-university/component-core-directives-pipes/component-core-directives-pipes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { COURSES } from 'src/data/db-data';
 
 @Component({
   selector: 'app-component-core-directives-pipes',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./component-core-directives-pipes.component.css']
 })
 export class ComponentCoreDirectivesPipesComponent {
+  coreCourse = COURSES[0];
+
+  rxjsCourse = COURSES[1];
+
+  ngRxCourse = COURSES[2];
 }
--- src/app/projects/angular-university/component-core-directives-pipes/course-card/course-card.component.ts
+++ src/app/projects/angular-university/component-core-directives-pipes/course-card/course-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-course-card',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-card.component.css']
 })
 export class CourseCardComponent {
-
+  @Input({
+    required: true
+  }) course: Course;
 }
