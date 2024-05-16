6e604fec3a73821202a7a22314bb798e0f25529f
ken0901 <ken5260715@gmail.com>
2024-05-15 21:51:59 -0700

    ngIf Directive and the Elvis Operator

--- src/app/projects/angular-university/component-core-directives-pipes/course-card/course-card.component.ts
+++ src/app/projects/angular-university/component-core-directives-pipes/course-card/course-card.component.ts
@@ -21,4 +21,8 @@ export class CourseCardComponent {
     console.log("card component - button clicked");
     this.courseSelected.emit(this.course);
   }
+
+  isImageVisible() {
+    return this.course && this.course.iconUrl;
+  }
 }
